6fc645c05c73e0e8f4647921f8a9672a6bdca69b
TrojanerHD <github@trojaner.dev>
2022-11-24 22:11:37 +0100

    Refactor main entry point file
    Use argv to determine execution of days

--- src/index.ts
+++ src/index.ts
@@ -3,45 +3,47 @@ import * as fs from 'fs';
 export type Response = Array<{ message: string; value: string }> | undefined;
 
 class Day {
-  private _year: string;
-  private _day: string;
+  static readonly #year: string = '2022';
+  #day: string;
 
-  constructor(year: number, day: number) {
-    this._year = year.toString();
-    this._day = day.toString();
-    if (day <= 9) this._day = `0${this._day}`;
+  constructor(day: number) {
+    this.#day = day.toString();
+    if (day <= 9) this.#day = `0${this.#day}`;
   }
 
-  init() {
-    fs.readFile(
-      `./src/${this._year}/${this._day}/values.txt`,
-      'utf8',
-      this.fileRead.bind(this)
+  async start(): Promise<void> {
+    const data: string = fs.readFileSync(
+      `./src/${Day.#year}/${this.#day}/values.txt`,
+      'utf8'
     );
-  }
+    process.chdir(`./src/${Day.#year}/${this.#day}`);
 
-  private fileRead(err: NodeJS.ErrnoException | null, data: string): void {
-    if (err) {
-      console.error(err);
-      return;
+    const callbackFunction: { main: (data: string) => Response } = await import(
+      `./${Day.#year}/${this.#day}/index`
+    );
+
+    const toLog: Response = callbackFunction.main(data);
+
+    process.chdir('../../');
+    if (!toLog) return;
+    for (let i = 0; i < toLog.length; i++) {
+      const log = toLog[i];
+      this.logger(i + 1, log.message, log.value);
     }
-    process.chdir(`./src/${this._year}/${this._day}`);
-    import(`./${this._year}/${this._day}/index`).then(
-      (callbackFunction: { main: (data: string) => Response }) => {
-        const toLog: Response = callbackFunction.main(data);
-        process.chdir('../../');
-        if (!toLog) return;
-        for (let i = 0; i < toLog.length; i++) {
-          const log = toLog[i];
-          this.logger(i + 1, log.message, log.value);
-        }
-      }
-    );
   }
 
   private logger(part: number, message: string, value: string): void {
     console.log(
-      `[Year ${this._year}, Day ${this._day}, Part ${part}] ${message}: ${value}`
+      `[Year ${Day.#year}, Day ${this.#day}, Part ${part}] ${message}: ${value}`
     );
   }
 }
+
+async function main() {
+  for (const arg of process.argv.filter((_, i): boolean => i > 1)) {
+    if (!isNaN(Number(arg))) await new Day(Number(arg)).start();
+    else console.error(`Parameter ${arg} is not a number`);
+  }
+}
+
+main();
