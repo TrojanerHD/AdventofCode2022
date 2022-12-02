4d688671ca7c17b02bfc27bf0463a78b82637efc
TrojanerHD <github@trojaner.dev>
2022-12-02 06:35:10 +0100

    Add solutions for day 02

---
+++ src/02/index.ts
@@ -0,0 +1,89 @@
+import Day from '../day';
+import { Response } from '../index';
+
+export default class Day02 implements Day {
+  main(data: string): Response {
+    const split: string[] = data.split('\n');
+    let points = 0;
+    let pointsTwo = 0;
+    for (const value of split) {
+      const both = value.split(' ');
+      const opponent = both[0];
+      let you = both[1];
+      let stop = false;
+      switch (you) {
+        case 'X':
+          you = 'A';
+          points++;
+          switch (opponent) {
+            case 'A':
+              pointsTwo += 3;
+              break;
+            case 'B':
+              pointsTwo++;
+              break;
+            case 'C':
+              pointsTwo += 2;
+              points += 6;
+              stop = true;
+              break;
+          }
+          break;
+        case 'Y':
+          you = 'B';
+          switch (opponent) {
+            case 'A':
+              points += 6;
+              pointsTwo++;
+              stop = true;
+              break;
+            case 'B':
+              pointsTwo += 2;
+              break;
+            case 'C':
+              pointsTwo += 3;
+              break;
+          }
+          pointsTwo += 3;
+          points += 2;
+          break;
+        case 'Z':
+          you = 'C';
+          points += 3;
+          switch (opponent) {
+            case 'A':
+              pointsTwo += 2;
+              break;
+            case 'B':
+              points += 6;
+              pointsTwo += 3;
+              stop = true;
+              break;
+            case 'C':
+              pointsTwo++;
+              break;
+          }
+          pointsTwo += 6;
+          break;
+      }
+      if (stop) continue;
+
+      if (you === opponent) {
+        points += 3;
+        continue;
+      }
+    }
+    return [
+      {
+        message:
+          'If choosing what is on the strategy guide, you get these points',
+        value: points.toString(),
+      },
+      {
+        message:
+          'If choosing what to play according to the strategy guide, you get these points',
+        value: pointsTwo.toString(),
+      },
+    ];
+  }
+}
