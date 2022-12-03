8739feca7e908e90349f0dc5c7a50096a5fb4ff3
TrojanerHD <github@trojaner.dev>
2022-12-03 06:36:07 +0100

    Add more types to day 3

--- src/03/index.ts
+++ src/03/index.ts
@@ -3,26 +3,28 @@ import { Response } from '../index';
 
 export default class Day03 implements Day {
   main(data: string): Response {
-    const split = data.split('\n');
-    let result1 = 0;
-    let result2 = 0;
+    const split: string[] = data.split('\n');
+    let result1: number = 0;
+    let result2: number = 0;
     for (const line of split) {
-      const rucksack1 = line.slice(0, line.length / 2);
-      const rucksack2 = line.slice(line.length / 2);
+      const rucksack1: string = line.slice(0, line.length / 2);
+      const rucksack2: string = line.slice(line.length / 2);
 
-      const item = this.checkTwoRucksacks(rucksack1, rucksack2).split('')[0];
+      const item: string = this.checkTwoRucksacks(rucksack1, rucksack2).split(
+        ''
+      )[0];
       result1 += this.calculateItem(item);
     }
-    for (let i = 0; i < split.length; i += 3) {
-      const rucksacks = [split[i], split[i + 1], split[i + 2]];
-      const item = this.determineItems(rucksacks)[0];
+    for (let i: number = 0; i < split.length; i += 3) {
+      const rucksacks: string[] = [split[i], split[i + 1], split[i + 2]];
+      const item: string = this.determineItems(rucksacks)[0];
       result2 += this.calculateItem(item);
     }
     return [result1, result2];
   }
 
-  private determineItems(rucksacks: string[]) {
-    let lastItems = rucksacks[0];
+  private determineItems(rucksacks: string[]): string {
+    let lastItems: string = rucksacks[0];
     for (let i = 1; i < rucksacks.length; i++) {
       lastItems = this.checkTwoRucksacks(lastItems, rucksacks[i]);
     }
@@ -30,7 +32,7 @@ export default class Day03 implements Day {
   }
 
   private checkTwoRucksacks(rucksack1: string, rucksack2: string): string {
-    const items = [];
+    const items: string[] = [];
     for (const item of rucksack1.split('')) {
       if (rucksack2.split('').some((item2) => item === item2)) {
         if (!items.includes(item)) items.push(item);
@@ -40,7 +42,7 @@ export default class Day03 implements Day {
   }
 
   private calculateItem(item: string): number {
-    let subtract = 96;
+    let subtract: number = 96;
     if (item === item.toUpperCase()) subtract = 38;
     return item.charCodeAt(0) - subtract;
   }
